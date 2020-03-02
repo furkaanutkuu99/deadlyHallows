37d9ebddcf3c77a82b8d2508d24fc092df42e316
furkaanutkuu99 <61404711+furkaanutkuu99@users.noreply.github.com>
2020-03-02 11:39:05 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿
+    using System; 
+using System.Collections.Generic; 
+using System.Linq; 
+using System.Text; 
+using System.Threading.Tasks; 
+
+class Program { 
+    static void Main(string[] args) 
+    {
+        /* Console.WriteLine(); //yazma işlemi bittikten sonra imleç bir alt satıra geçer.
+         Console.WriteLine();
+         Console.WriteLine("Default Background Color: {0}", 
+                           Console.BackgroundColor); 
+       
+         Console.BackgroundColor 
+             = ConsoleColor.Blue
+             ; 
+       
+         Console.WriteLine("Changed Background Color: {0}", 
+                           Console.BackgroundColor);
+         Console.Write(Environment.NewLine); //yeni bir satır eklemek için 
+         Console.Write("Adınız:");
+         Console.WriteLine( Console.ReadLine()); //console.readlinegeriye str bir değer döndürürken console.read geriye int bir değre döndürür.
+         Console.Write("Adınız:");
+         Console.WriteLine(Console.ReadKey()); //readKEy geriye tek bir değer döndürür.
+        ConsoleKeyInfo asd = Console.ReadKey();
+        asd. // asd adlı bir değişkene atayıp nokta operatörüyle alabileceği değerlere ulaşabiliyoruz.
+        console.writeLine(char(65));
+        Console.write(int('A)); //tek karakter kullanıyorsak tek tırnak kullanırız
+     
+        string a;
+        a = "ahmet";
+        Console.WriteLine("Adınız:" + a);
+    }    */
+
+
+} 
